3ef376da2cc4c4307f2d6494e13063be6166bb94
Hugo Souza <hugosr.contato@gmail.com>
2024-05-31 16:25:16 -0300

    feat: create wallet section

--- web/src/views/DashboardView.vue
+++ web/src/views/DashboardView.vue
@@ -1,6 +1,41 @@
 <script setup lang="ts">
+import BalanceToggle from '@/components/BalanceToggle.vue';
+import { ref } from 'vue';
+
+const balance = ref(0)
 </script>
 
 <template>
-  <div></div>
+  <div class="dashboard">
+    <header class="greeting-header">
+      <div>Olá, bom dia!</div>
+    </header>
+    
+    <section class="wallet">
+      <h2>Sua Carteira</h2>
+      <div class="content">
+        <BalanceToggle :balance="balance"/>
+      </div>
+    </section>
+  </div>
 </template>
+
+<style scoped>
+.dashboard {
+  padding: 2rem;
+}
+
+.greeting-header {
+  margin-top: 48px;
+  font-size: 1rem;
+}
+
+.wallet {
+  margin: 32px 0;
+}
+
+.wallet h2 {
+  color: #fff;
+  margin-bottom: 16px
+}
+</style>
